dd01d69120750082551c1f86eab21b7651e88f61
David Yang <yangd@pinkelephant.com>
2023-03-31 14:05:41 -0400

    added links to mockup

--- src/components/navigations/navItems.component.jsx
+++ src/components/navigations/navItems.component.jsx
@@ -26,7 +26,7 @@ export default function NavItems () {
             ]
         }
     ]
-
+// * https://dribbble.com/shots/20108403-Odasta-Admin-Dashboard
     return <div className="mt-6 p-3">
         {menuItems.map(item => (<NavSections section = {item} key = {item.type}/>))}
     </div>
