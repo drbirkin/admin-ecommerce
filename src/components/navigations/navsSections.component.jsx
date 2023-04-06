aa92e53d1a248d0ff918fdf8b3804b8b65b625fd
David Yang <yangd@pinkelephant.com>
2023-04-06 13:56:41 -0400

    fixed submenu styling

--- src/components/navigations/navsSections.component.jsx
+++ src/components/navigations/navsSections.component.jsx
@@ -16,28 +16,28 @@ export default function NavSections({ section: { type, items, icons } }) {
                 to={`/${menu !== 'home' ? menu : ''}`}
                 className={({ isActive, isPending }) =>
                   isPending
-                    ? 'pending font-light mb-3 flex items-center p-2 pl-6 relative'
+                    ? 'pending font-light mt-3 flex items-center p-2 pl-6 relative'
                     : isActive
-                    ? 'active bg-indigo-100 text-indigo-500 font-regular mb-3 flex items-center p-2 pl-6 rounded relative group'
-                    : 'group text-black mb-3 flex items-center p-2 pl-6 relative'
+                    ? `active ${subitems?.length ? 'submenu-active' : ''} bg-indigo-100 text-indigo-500 font-regular mt-3 flex items-center p-2 pl-6 rounded relative group peer`
+                    : 'group text-black mt-3 flex items-center p-2 pl-6 relative'
                 }
                 key={menu}
               >
                 <Icon className="text-lg text-slate-800 group-hover:text-indigo-500" />
                 <p className="ml-4 group-hover:text-indigo-500">{menu}</p>
                 {subitems?.length && 
-                  <RiArrowDropDownLine className='absolute right-2 text-3xl transition-all ease-linear group-[.active]:rotate-180'/>
+                  <RiArrowDropDownLine className='absolute text-slate-400 right-2 text-3xl transition-all ease-linear group-[.active]:rotate-180'/>
                 }
               </NavLink>
 
               {subitems?.length && (
-                <div className='text-left mb-6'>
+                <div  x-ref={menu} className={`text-left max-h-0 overflow-hidden transition-all ease-in-out duration-300	peer-[.submenu-active]:max-h-44`}>
                   {subitems.map(({menu}, index) => <NavLink to = {`products/${index ? menu : ''}`} 
                   className={({ isActive, isPending }) =>
                     isPending
                       ? 'pending font-light flex items-center p-2 pl-20'
                       : isActive
-                      ? 'active  text-indigo-500 font-regular flex items-center p-2 pl-20 rounded'
+                      ? 'active text-indigo-500 font-regular flex items-center p-2 pl-20 rounded'
                       : 'group text-slate-700 flex items-center p-2 pl-20'
                   }
                   key={menu}>
